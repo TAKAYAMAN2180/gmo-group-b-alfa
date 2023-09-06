50e2a11813db2031f54597c001c8b7490c91c753
takayaman2180 <beruberudog.27@gmail.com>
2023-09-06 11:45:06 +0900

    implement login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import {signIn, useSession} from "next-auth/react";
 
 export default function Page() {
-  return (
-    <>
-    <h1>hello</h1>
-    <p>world</p>
-    </>
-  )
+    const {data: session} = useSession();
+
+
+    return (
+        <>
+            <div className={"d-flex justify-content-center"}>
+                <div className={"w-75 rounded border rounded-3 border-2 border-primary d-flex flex-column justify-content-center text-center p-3"}
+                     style={{marginTop: "25vh", alignItems: "center"}}>
+                    <div className={"fs-3 border-primary border-start border-3 ps-3"}>ログイン</div>
+                    <div className={"w-100 mt-5"}>下のボタンを押してGoogleアカウントにログインしてください</div>
+                    {!session &&
+                        (<div className={"w-50 mx-auto"}>
+                            <img className={"img-fluid"} src={"./btn_google_signin_dark_normal_web@2x.png"}
+                                 onClick={() => signIn("google")}/>
+                        </div>)}
+                </div>
+            </div>
+
+
+        </>
+    )
 }
