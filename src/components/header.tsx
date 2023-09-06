2299b96e34a95830602b0920fae7855b759a9787
harukaAsano01 <k21rs003@gmail.com>
2023-09-06 14:55:27 +0900

    add-header-design

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,22 @@
 export default function Header() {
   return (
     <>
-    <h1>here is header.</h1>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div className="container-fluid p-2">
+          <div className="">
+            <a className="navbar-brand ms-3" href="/home">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+          <div className="justify-content-md-end collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link mx-2" href="#">Username</a>
+              <a className="nav-link btn mx-2 btn-outline-secondary" href="#">Logout</a>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
